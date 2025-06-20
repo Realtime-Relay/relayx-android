631774d0be631d82822119337b2ac7465ce51753
Krupa Sanghani <krupasanghani1@gmail.com>
2025-06-20 17:19:04 +0530

    Initial Nats - SDK

---
+++ app/src/main/java/com/angistudio/nata_sdk/RealTimeSDK.kt
@@ -0,0 +1,34 @@
+package com.angistudio.nata_sdk
+
+import io.nats.client.Connection
+import io.nats.client.Nats
+import io.nats.client.Options
+import java.nio.charset.StandardCharsets
+
+class Realtime(private val apiKey: String, private val secretKey: String) {
+
+    private var connection: Connection? = null
+
+    init {
+        require(apiKey.isNotBlank()) { "apiKey must not be empty" }
+        require(secretKey.isNotBlank()) { "secretKey must not be empty" }
+    }
+
+    fun connect(natsUrl: String = "nats://api.relay-x.io:4222") {
+        val opts = Options.Builder()
+            .server(natsUrl)
+            .userInfo(apiKey.toCharArray(), secretKey.toCharArray()) // Or use .token()
+            .build()
+
+        connection = Nats.connect(opts)
+    }
+
+    fun publish(subject: String, message: String) {
+        val conn = connection ?: throw IllegalStateException("Not connected to NATS")
+        conn.publish(subject, message.toByteArray(StandardCharsets.UTF_8))
+    }
+
+    fun close() {
+        connection?.close()
+    }
+}
